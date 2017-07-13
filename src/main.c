5a3542a5c1f0ccb8fafdc350218740a9a7131e35
Nisarg Patel <npatel33@gmu.edu>
2017-07-12 20:50:20 -0400

    Called gstreamer_setup function to capture an image.

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "recorder/gstreamer.h"
+
+/**
+* @fn main(int argc, char *argv[])
+* @brief main entry function
+*/
+int main(int argc, char *argv[])
+{
+    /* Initialize GStreamer */
+    gst_init(&argc, &argv);
+	gstreamer_setup();
+	return 0;
+}
