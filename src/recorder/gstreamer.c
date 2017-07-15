4d6d1e40b9b44b0aaf74b716a105fbc376a401d4
Nisarg Patel <npatel33@gmu.edu>
2017-07-14 22:00:21 -0400

    add spaces to indentation

--- src/recorder/gstreamer.c
+++ src/recorder/gstreamer.c
@@ -16,16 +16,17 @@
 */
 static gboolean bus_callback(GstBus *bus, GstMessage *msg, gpointer data)
 {
-    g_print("Got %s message\n", GST_MESSAGE_TYPE_NAME(msg));
-
-    switch (GST_MESSAGE_TYPE(msg)) {
-        case GST_MESSAGE_EOS:
-            g_main_loop_quit(loop);
-            break;
-
-        default:
-            break;
-    }
+	g_print("Got %s message\n", GST_MESSAGE_TYPE_NAME(msg));
+	
+	switch (GST_MESSAGE_TYPE(msg)) {
+		
+		case GST_MESSAGE_EOS:
+			g_main_loop_quit(loop);
+			break;
+			
+		default:
+			break;
+	}
 	return 0;
 }
 
@@ -36,7 +37,7 @@ static gboolean bus_callback(GstBus *bus, GstMessage *msg, gpointer data)
 */ 
 int gstreamer_setup(void)
 {
-    
+
 	/* create pipeline  */
     pipeline = gst_pipeline_new("my-pipeline");
     if (!pipeline) {
@@ -48,7 +49,7 @@ int gstreamer_setup(void)
     bus = gst_pipeline_get_bus(GST_PIPELINE (pipeline));
     bus_watch_id = gst_bus_add_watch(bus, bus_callback, NULL);
     gst_object_unref(bus);
-    
+
 	/* create application elements */
     camera = gst_element_factory_make("v4l2src", "camera");
 	video_converter = gst_element_factory_make("videoconvert", "video_converter");
@@ -62,16 +63,16 @@ int gstreamer_setup(void)
         g_print("Failed to create one or more elements!\n");
         return -1;
     }
-    
+
     //g_object_set(G_OBJECT (camera), "num-buffers", 1, NULL);
 
     /* set ouput image location */
     g_object_set(G_OBJECT (video_writer), "location", "capture.avi", NULL);
-    
+
     caps = gst_caps_from_string("video/x-raw,framerate=30/1");
 
     g_object_set(G_OBJECT (caps_filter), "caps", caps, NULL);
-    
+
 	/* add elements to pipeline */
     gst_bin_add_many(GST_BIN (pipeline), camera, caps_filter, video_converter, img_enc,
 			video_enc, video_writer, NULL);
@@ -81,7 +82,7 @@ int gstreamer_setup(void)
         g_print ("Failed to link elements\n");
         return -1;
     }
-    
+
     /* start capture */
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
