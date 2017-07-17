e399d9710c6ecc9d00537433f72286adb0bc4867
achopra6 <achopra6@masonlive.gmu.edu>
2017-07-17 02:19:43 -0400

    added multithreading, stubs for threads, console reader.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,80 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <stddef.h>
 #include "recorder/gstreamer.h"
+
+/**
+* @file main.c
+* @brief Contains routines to create multi threads
+* @author Abhimanyu Chopra
+*/
+
+enum user_command{
+	NONE,
+	START,
+	PAUSE,
+	STOP,
+	EXIT
+}status;
+
+
+/**
+* @fn video_capture(void *nothing)
+* @brief function to setup and initialize GStreamer
+*/
+void* video_capture(void *nothing){
+	/* Initialize GStreamer */
+	gst_init(NULL,NULL);
+	//gst_init(&argc, &argv);	//Do we need the arguments ?
+	gstreamer_setup();
+	return NULL;
+}
+
+/**
+* @fn console_reader(void *nothing)
+* @brief function to read from console
+*/
+void* console_reader(void *nothing){
+	
+	char key = 'a';
+	status = NONE;
+	
+	printf("Welcome to the Raspberry Pie Action Cam. \nUser command options \nStart - s \nPause - p \nStop - o \nExit - e \n");
+
+	while(key!='e'){
+		printf("Enter command: ");
+		scanf(" %c", &key);
+		if(key == 's')
+			status = START;
+		else if(key == 'p')
+			status = PAUSE;
+		else if(key == 'o')
+			status = STOP;
+		else if(key == 'e')
+			status = EXIT;
+		else
+			printf("Unknown command.\n");
+		
+	}
+
+	return NULL;
+}
+
+/**
+* @fn secure_server(void *nothing)
+* @brief function to host secure server
+*/
+void* secure_server(void *nothing){
+
+	// call server related functions
+	return NULL;
+}
 
 /**
 * @fn main(int argc, char *argv[])
@@ -7,9 +82,29 @@
 */
 int main(int argc, char *argv[])
 {
-	/* Initialize GStreamer */
-	gst_init(&argc, &argv);
-	gstreamer_setup();
-	
+	pthread_t tid[10];
+
+	// create console reader thread
+	if((errno = pthread_create(&(tid[0]), NULL, &console_reader, NULL))!=0){
+		fprintf(stderr,"Error creating console reader thread. Error Code:%d %s\n",errno,strerror(errno) );
+		exit(EXIT_FAILURE);
+	}
+
+	// create video capture thread
+	if((errno = pthread_create(&(tid[1]), NULL, &video_capture, NULL))!=0){
+		fprintf(stderr,"Error creating video capture thread. Error Code:%d %s\n",errno,strerror(errno) );
+		exit(EXIT_FAILURE);
+	}
+
+	// wait for threads to exit
+	for(int i=0;i<10;i++){
+		pthread_join(tid[i],NULL);
+	}
+
+	printf("Thank You. Bye!\n");
+
 	return 0;
 }
+
+
+
